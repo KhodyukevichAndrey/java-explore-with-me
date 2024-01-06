a23e95b220de486c1d8249351b9cc07a92449f0b
Андрей Ходюкевич <Lolds456@yandex.ru>
2024-01-06 17:03:47 +0400

    Grade Project v1.1 - reworked dto model - optimized pom's - add log's

--- statistic/dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
+++ statistic/dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
@@ -3,13 +3,16 @@ package ru.practicum.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class EndpointHitDto {
     @NotBlank
     @Size(max = 50)
@@ -20,6 +23,7 @@ public class EndpointHitDto {
     @NotBlank
     @Size(max = 50)
     private String ip;
+    @NotNull
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
